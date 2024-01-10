f5c88267d2176eb30bd91de52c5635e985aaf7dc
Logan Williams <logan@fireflysoftware.dev>
2024-01-10 22:41:39 +0000

    Basic markdown processing addded for /posts

---
+++ src/lib/Post.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { ChevronRightIcon } from 'lucide-svelte';
+	import { formatDate } from '$lib/utils';
+	export let published: boolean = false;
+	export let categories: string[] = [];
+	export let title: string = 'Card Title';
+	export let slug: string = '#';
+	export let date: string = '2021-10-10';
+	export let description: string =
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique quis te';
+	$: datetime = formatDate(date);
+</script>
+
+<article class="group relative flex flex-col items-start">
+	<h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+		<!-- Hover effect on card -->
+		<div
+			class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-100 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
+		/>
+		<a href={`/posts/${slug}`}>
+			<span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
+			<span class="relative z-10">{title}</span>
+		</a>
+	</h2>
+	<time
+		class="relative z-10 order-first mb-3 flex items-center pl-3.5 text-sm text-zinc-400 dark:text-zinc-500"
+		datetime={date}
+	>
+		<span aria-hidden="true" class="absolute inset-y-0 left-0 flex items-center">
+			<span class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+		</span>
+		{datetime}
+	</time>
+	<p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">{description}</p>
+	<div
+		aria-hidden="true"
+		class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
+	>
+		Read article
+		<ChevronRightIcon class="ml-1 h-3 w-3" />
+	</div>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { formatDate } from '$lib/utils';
 	import Container from '$lib/container/index.svelte';
 	import SocialLink from '$lib/SocialLink.svelte';
 	import Github from 'lucide-svelte/icons/github';
@@ -7,7 +8,7 @@
 	import Twitter from 'lucide-svelte/icons/twitter';
 	import Instagram from 'lucide-svelte/icons/instagram';
 	// import Photos from '$lib/spotlight/components/Photos.svelte';
-	// import Post from '$lib/spotlight/components/Post.svelte';
+	import Post from '$lib/Post.svelte';
 	// import Newsletter from '$lib/spotlight/components/Newsletter.svelte';
 	// import Resume from '$lib/spotlight/components/Resume.svelte';
 
@@ -16,6 +17,10 @@
 	const title = "Web developer, air traffic controller, and amateur writer."
 	const description = " I'm Logan, a freelance web developer chilling in Helena, MT. This cozy space on the web is where I share my thoughts, articles, and projects. My goal? To make the web not just functional but downright lovely, especially for those who might find technology a bit tricky in our fast-paced modern world."
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 
 <Container cls="mt-24">
 	<div class="max-w-2xl">
@@ -31,16 +36,16 @@
 	</div>
 </Container>
 <!-- <Photos /> -->
-<!-- <Container cls="mt-24 md:mt-28">
-	<div class="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+<Container cls="mt-24 md:mt-28">
+	<div class="mx-auto grid grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
 		<div class="flex flex-col gap-16">
 			{#each data.posts as post}
 				<Post {...post} />
 			{/each}
 		</div>
-		<div class="space-y-10 lg:pl-16 xl:pl-24">
+		<!-- <div class="space-y-10 lg:pl-16 xl:pl-24">
 			<Newsletter />
 			<Resume />
-		</div>
+		</div> -->
 	</div>
-</Container> -->
+</Container>
